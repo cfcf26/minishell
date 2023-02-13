5b30b5922e2a93d9fe82ff78e7722e841a5bd00b
ekwak <ekwak@student.42seoul.kr>
2023-02-13 20:04:48 +0900

    delete init_envp & init_signal

--- init_signal.c
+++
@@ -1,8 +0,0 @@
-#include "minishell.h"
-
-
-void	init_signal(void)
-{
-	signal(SIGINT, SIG_IGN);  // 컨씨
-	signal(SIGQUIT, SIG_IGN); //컨 역슬레쉬
-}
--- src/init_signal.c
+++ src/init_signal.c
@@ -2,5 +2,6 @@
 
 void	init_signal(void)
 {
-	// TODO
+	signal(SIGINT, SIG_IGN);  // 컨씨
+	signal(SIGQUIT, SIG_IGN); //컨 역슬레쉬
 }
