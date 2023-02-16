c918438291c571afd42a9713b99226db69c978f6
yonshin <yonshin@student.42seoul.kr>
2023-02-16 21:25:42 +0900

    builtin echo
    builtin echo fix

--- src/builtin/builtin_echo.c
+++ src/builtin/builtin_echo.c
@@ -3,6 +3,24 @@
 
 int	builtin_echo(t_list *argv)
 {
-	(void)argv;
-	return (1);
+	int	nextline;
+
+	argv = argv->next;
+	nextline = 1;
+	if (argv)
+	{
+		if (ft_strncmp("-n", argv->content, 3) == 0)
+			nextline = 0;
+		else
+			printf("%s", argv->content);
+		argv = argv->next;
+	}
+	while (argv)
+	{
+		printf(" %s", argv->content);
+		argv = argv->next;
+	}
+	if (nextline)
+		printf("\n");
+	return (0);
 }
