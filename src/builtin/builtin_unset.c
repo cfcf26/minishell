2ae67ee3dadc3818e9ed9287d0dd1a0c09faa480
yonshin <yonshin@student.42seoul.kr>
2023-02-16 22:15:18 +0900

    builtin unset
    builtin unset
    
    builtin unset

--- src/builtin/builtin_unset.c
+++ src/builtin/builtin_unset.c
@@ -1,7 +1,26 @@
+#include "utils.h"
 #include "builtin.h"
 
 int	builtin_unset(t_list *argv)
 {
-	(void)argv;
-	return (1);
+	char		*key;
+
+	argv = argv->next;
+	while (argv)
+	{
+		key = argv->content;
+		if (ft_iscontain(ENV_CHARSET, key) == 0)
+		{
+			// TODO: error
+			/*
+				bash-3.2$ unset !
+				bash: unset: `!': not a valid identifier
+			*/
+			ft_putstr_fd("error", 2);
+			continue ;
+		}
+		set_env(key, NULL);
+		argv = argv->next;
+	}
+	return (0);
 }
