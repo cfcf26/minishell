735f8ebef127437eab36edfd93848dfe5afc18f5
yonshin <yonshin@student.42seoul.kr>
2023-02-17 01:51:01 +0900

    builtin cd

--- src/builtin/builtin_cd.c
+++ src/builtin/builtin_cd.c
@@ -1,7 +1,15 @@
 #include "builtin.h"
+#include "model.h"
 
 int	builtin_cd(t_list *argv)
 {
-	(void)argv;
-	return (1);
+	argv = argv->next;
+	if (chdir(argv->content))
+	{
+		ft_putstr_fd("error", 2);
+		return (0);
+	}
+	free(data()->pwd);
+	data()->pwd = getcwd(NULL, 0);
+	return (0);
 }
